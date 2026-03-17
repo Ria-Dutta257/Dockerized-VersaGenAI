a0fe37bdaa77b2a2eeb90382662af0a4b46cf68c
Ria Dutta <riadutta257@gmail.com>
2026-03-17 10:10:04 +0530

    Initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AITools from '../components/AITools'
+import Testimonial from '../components/Testimonial'
+import Plans from '../components/Plans'
+import Footer from '../components/Footer'
+
+
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <Hero/> 
+    <AITools/>
+    <Testimonial/>
+    <Plans/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
